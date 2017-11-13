604ea28cf419cbe09e26f185f8caaf1adea62e2a
Marcelo Morais <marcelo.morais@symbio.com>
2017-11-13 14:41:22 +0100

    Adding history component
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -5,7 +5,7 @@ import { purple } from '../utils/colors'
 
 export default function DateHeader ({ date }) {
   return (
-    <Text style={{color: purple, fontSize: 25}}>{date}</Text>
+      <Text style={{color: purple, fontSize: 25}}>{date}</Text>
   )
 
 }

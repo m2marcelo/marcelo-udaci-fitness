eeef98f0015d701516c1430461bb827552b969a6
Marcelo Morais <marcelo.morais@symbio.com>
2017-11-13 14:58:25 +0100

    Adding calendar component
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

--- components/History.js
+++ components/History.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions'
 import { fetchCalendarResults } from '../utils/api'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import UdaciFitnessCalendar  from 'udacifitness-calendar'
 
 
 class History extends Component {
@@ -21,11 +22,30 @@ class History extends Component {
       .then(() => this.setState(() => ({ready: true})))
   }
 
-  render() {
+  renderItem = ({ today, ...metrics}, formattedDate, key) => (
+    <View>
+      {today
+        ? <Text>{JSON.stringify(today)}</Text>
+        : <Text>{JSON.stringify(today)}</Text>}
+    </View>
+  )
+
+  renderEmptyDate(formattedDate) {
     return (
       <View>
-      <Text>{JSON.stringify(this.props)}</Text>
+        <Text>No data for this day</Text>
       </View>
+    )
+  }
+
+  render() {
+    const { entries } = this.props
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
     )
   }
 }

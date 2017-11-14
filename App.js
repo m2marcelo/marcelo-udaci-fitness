8f1c55b1e9170507fa203c2dc8118cc3e0e12965
Marcelo Morais <marcelo.morais@symbio.com>
2017-11-14 16:25:04 +0100

    Adding TabNavigator
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   View,
   Text,
+  Platform,
   TouchableHighlight, //changes the color of an elemente when it is pressed
   TouchableNativeFeedback, // for android, it gives the ripple effect
   TouchableOpcaity, //changes the opacity of an elemente when it is pressed
@@ -10,8 +11,49 @@ import {
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { TabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import reducer from './reducers'
 import History from './components/History'
+
+const Tabs = TabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+},
+{
+  navigationOptions: {
+    header: null
+  }
+},
+{
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
 
@@ -20,7 +62,7 @@ export default class App extends React.Component {
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
           <View style={{height: 20}}/>
-          <History />
+            <Tabs />
         </View>
       </Provider>
     )

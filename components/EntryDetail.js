ee14e8cfd952cb0394429ff53637ef55edb19117
Marcelo Morais <marcelo.morais@symbio.com>
2017-11-16 16:35:49 +0100

    Updating reset method for addentry details
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { removeEntry } from '../utils/api'
+import { white } from '../utils/colors'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import MetricCard from './MetricCard'
 import TextButton from './TextButton'
 
 class EntryDetail extends Component {
@@ -18,16 +21,23 @@ class EntryDetail extends Component {
     }
   }
 
-reset = () => {
-  const { remove, goBack, entryId } =  this.props
-  remove()
-  goBack()
-  removeEntry(entryId)
-}
+  reset = () => {
+    const { remove, goBack, entryId } =  this.props
+    remove()
+    goBack()
+    removeEntry(entryId)
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metrics !== null && !nextProps.metrics.today
+  }
 
   render () {
+    const { metrics } = this.props
+
     return (
-      <View>
+      <View style={styles.container}>
+        <MetricCard metrics={metrics}/>
         <TextButton onPress={this.reset} style={{margin: 20}}>
           RESET
         </TextButton>
@@ -36,10 +46,27 @@ reset = () => {
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  }
+})
+
+function mapStateToProps(state, { navigation }) {
+  const { entryId } = navigation.state.params
+
+  return {
+    entryId,
+    metrics: state[entryId]
+  }
+}
+
 function mapDispatchToProps(dispatch, { navigation }) {
   const { entryId } = navigation.state.params
   return {
-    remove: () => dispatch(AddEntry({
+    remove: () => dispatch(addEntry({
       [entryId]: timeToString() === entryId
         ? getDailyReminderValue()
         : null
@@ -48,4 +75,4 @@ function mapDispatchToProps(dispatch, { navigation }) {
   }
 }
 
-export default EntryDetail
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail)
